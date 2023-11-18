f92ceb81c67a0c2dddaf9bd68e5c5d32f2af48f8
Muhammad-Dava <mdava@students.amikom.ac.id>
2023-11-18 23:14:29 +0700

    firts commit

--- app.py
+++ app.py
@@ -1,10 +1,63 @@
-from flask import Flask
+from flask import Flask, render_template, request, flash, url_for, redirect
+from mysql import connector
 
 app = Flask(__name__)
+app.secret_key = 'frankmakanayam'  
+
+db = connector.connect (
+    host = 'localhost',
+    user = 'root',
+    passwd = '',
+    database = 'db_kelompok2'
+)
+
+if db.is_connected():
+    print("berhasil terhubung ke database")
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello, World!, Iza Ganteng</h1>"
+def home():
+    cursor = db.cursor()
+    query = "SELECT nama FROM tbl_anggota"
+    cursor.execute(query)
+    results = cursor.fetchall()  # Ambil semua baris hasil
+    cursor.close()
+
+    # Ambil semua nama dari hasil query
+    nama_anggota = [result[0] for result in results] if results else None
+
+    # Kirim hasil ke template HTML
+    return render_template('index.html', hasil=nama_anggota)
+
+@app.route("/anggota")
+def anggota():
+    return render_template('anggota.html')
+
+@app.route("/edit")
+def edit():
+    return render_template('edit.html')
+
+@app.route('/ubah-nama/<int:id>', methods=['GET', 'POST'])
+def ubah_nama(id):
+    if request.method == 'POST':
+        nama_baru = request.form['nama_baru1']
+        
+        cursor = db.cursor()
+        query = "UPDATE tbl_anggota SET nama = %s WHERE no = %s"
+        cursor.execute(query, (nama_baru, id))
+        db.commit()
+        cursor.close()
+
+        if request.method == 'POST':
+            nama = request.form.get('nama_baru1')
+        if not nama:
+            flash('Nama tidak boleh kosong', 'error')
+        else:
+            flash('Data berhasil diubah', 'success')
+            # Lakukan operasi penyimpanan data atau operasi lain yang diperlukan
+            return redirect(url_for('edit'))
+
+    return render_template('edit.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Mendapatkan teks dari elemen dengan ID 'teks-slide1'
+    var teksSlide1 = document.getElementById('teks-slide1').innerText;
+  
+    // Menampilkan teks yang ditangkap di konsol (opsional, untuk keperluan uji)
+    console.log("Teks dari teks-slide1:", teksSlide1);
+  });
+  
+
+  
+// tombol clode 
+  $(".alert").alert('close')
